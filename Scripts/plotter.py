1abe03159aa4b7a1f30c3f7fd96ea6da097bd28e
dleyvacastro <dleyvacastro@gmail.com>
2022-07-22 22:34:18 -0500

    add new big plot

--- Scripts/plotter.py
+++ Scripts/plotter.py
@@ -37,16 +37,37 @@ def changesvtime(cndf : pd.DataFrame):
     plt.ylabel("Time (Segs)")
     plt.savefig('./plots/changesvtime.png')
 
+def bigplot(df : pd.DataFrame):
+    changes = [20,50,80,100]
+    css = [df[df['# Changes'] == i] for i in changes]
+    # 4 x 1 sub plot nodes vs iterations
+    fig, axs = plt.subplots(1, 4, figsize=(16,7))
+    for i, ax in enumerate(axs):
+        ax.plot(css[i]['# Nodes'], css[i]['Iterations'])
+        ax.set_title(f"{changes[i]} Changes.", pad = 10)
+        ax.set_xlabel("Nodes")
+        ax.set_ylabel("Iterations")
+    # get the max time
+    max_time = round(max([max(css[i]['Time']) for i in range(4)]))
+    # Name the figure
+    fig.suptitle("Nodes vs. Iterations. (20,50,80,100 Changes)")
+    # add a footnote
+    fig.text(0.5, 0.01, f"Max time {max_time//60} Minutes {max_time%60} Seconds", ha='center', fontsize=10)
+    plt.savefig('./plots/bigplot.png')
+
+
 def main():
     df = pd.read_csv('Data.csv')
-    ccdf = df[df['# Changes'] == 10]
-    cndf = df[df['# Nodes'] == 200]
-    nodevit(ccdf)
-    plt.clf()
-    nodevtime(ccdf)
-    plt.clf()
-    changesvtime(cndf)
-    # print(sum(df['Tiempo']))
+    #ccdf = df[df['# Changes'] == 10]
+    #cndf = df[df['# Nodes'] == 200]
+    #nodevit(ccdf)
+    #plt.clf()
+    #nodevtime(ccdf)
+    #plt.clf()
+    #changesvtime(cndf)
+    ## print(sum(df['Tiempo']))
+    bigplot(df)
 
 if "__main__" == __name__:
     main()
+    # bigplot()
